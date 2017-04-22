020a021cfcd6d5df2d521be78c27b551a063df11
gjvoit <gjvoit@yahoo.com>
2017-04-22 19:50:18 -0400

    Playing with quickSort

--- src/PracticeQuestions/Playground.java
+++ src/PracticeQuestions/Playground.java
@@ -7,9 +7,45 @@ import java.util.Arrays;
  */
 public class Playground {
     public static void main(String[] args) {
-        int[] arr = new int[10];
-        System.out.println(Arrays.toString(arr));
-        arr[5] = 6 ;
-        System.out.println(arr[5]);
+        Playground pg = new Playground();
+        int[] unsorted = {3, 1, 0, 4};
+        System.out.println(Arrays.toString(unsorted));
+        pg.quickSort(unsorted);
+        System.out.println(Arrays.toString(unsorted));
     }
+
+    void quickSort(int[] arr) {
+        quickSort(arr, 0, arr.length-1);
+    }
+
+    void quickSort(int[] arr, int left, int right) {
+        int index = partition(arr, left, right);
+        if (left < index-1) {
+            quickSort(arr, left, index-1);
+        }
+        if (index < right) {
+            quickSort(arr, index, right);
+        }
+    }
+
+    int partition(int[] arr, int left, int right) {
+        int pivot = arr[(left+right)/2];
+        while (left <= right) {
+            while (arr[left] < pivot) left++;
+            while (arr[right] > pivot) right--;
+            if (left <= right) {
+                swap(arr, left, right);
+                left++;
+                right--;
+            }
+        }
+        return left;
+    }
+
+    void swap(int[] arr, int left, int right) {
+        int temp = arr[right];
+        arr[right] = arr[left];
+        arr[left] = temp;
+    }
+
 }
